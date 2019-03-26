e6b8ac8a924a8fe94acda41a6af2169ca43eef1c
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-26 09:36:43 +0000

    added tests in and passed

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,4 +1,29 @@
 
 
 class SecretDiary
+attr_reader :locked, :diary
+
+  def initialize
+    @locked = true
+    @diary = []
+  end
+
+  def lock
+    @locked = true
+  end
+
+  def unlock
+    @locked = false
+  end
+
+  def add_entry(text)
+    fail "Diary Locked" if @locked == true
+    @diary << text
+  end
+
+  def get_entries
+    fail "Diary Locked" if @locked == true
+    return @diary
+  end
+
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -2,4 +2,30 @@ require 'secret_diary'
 
 describe SecretDiary do
 
+  it 'locks' do
+    subject.lock
+    expect(subject.locked).to eq true
+  end
+
+  it 'unlocks' do
+    subject.unlock
+    expect(subject.locked).to eq false
+  end
+
+  it 'can add entries' do
+    subject.unlock
+    subject.add_entry("Hi")
+    expect(subject.get_entries).to include "Hi"
+  end
+
+  it "can't add entries when locked" do
+    allow(subject).to receive(:locked).and_return(true)
+    expect { subject.add_entry("Hi") }.to raise_error "Diary Locked"
+  end
+
+  it "can't get entries when locked" do
+    allow(subject).to receive(:locked).and_return(true)
+    expect { subject.get_entries }.to raise_error "Diary Locked"
+  end
+
 end
